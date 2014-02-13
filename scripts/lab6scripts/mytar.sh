cf5c29c74e44a593412c5db5cd4d4167d468e3b2
nitish <nitishchandrachinta@gmail.com>
2014-02-13 16:52:08 +0530

    2nd question is completed

---
+++ scripts/lab6scripts/mytar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $1 -eq 1 ]
+then
+	tar -cf src.tar src
+#	echo "tarred"
+	gzip src.tar
+else
+	cp -r src dummy
+	cd dummy
+#	echo "zipped and tarred"
+	for i in $(ls)
+	do
+		gzip $i
+	done
+	cd ..
+	tar -cf src.tar dummy
+	rm -rf dummy
+fi
