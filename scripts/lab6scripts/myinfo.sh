1eed99a0eac962fdcf50daad4504d8a97e742cac
nitish <nitishchandrachinta@gmail.com>
2014-02-19 08:55:54 +0530

    this is not the submitted one

--- scripts/lab6scripts/myinfo.sh
+++ scripts/lab6scripts/myinfo.sh
@@ -3,14 +3,37 @@ ramgb=$(echo "scale=2; $(free | sed -n '2s/[^0-9]*\([0-9]*\).*/\1/p')/(1024.0*10
 usedgb=$(echo "scale=2; $(free | sed -n '2s/[^0-9]*[0-9]*[^0-9]*\([0-9]*\).*/\1/p')/(1024*1024)" | bc)
 #echo $ramgb
 #echo $usedgb
+#date=$(date +%d)
+#month=$(date +%m)
+year=$(date +%Y)
+#hour=$(date +%H)
+#minutes=$(date +%M)
+#presenttime
+dineshsec=$(($(date +%s -d ${year}0724)-$(date +%s -d today)))
+nitishsec=$(($(date +%s -d ${year}0419)-$(date +%s -d today)))
+mahisec=$(($(date +%s -d ${year}0808)-$(date +%s -d today)))
+if [ $dineshsec -lt 0 ]
+then
+dineshsec=$(($(date +%s -d $((${year}+1))0724)-$(date +%s -d today)))
+fi
+if [ $nitishsec -lt 0 ]
+then
+nitishsec=$(($(date +%s -d $((${year}+1))0419)-$(date +%s -d today)))
+fi
+if [ $mahisec -lt 0 ]
+then
+mahisec=$(($(date +%s -d $((${year}+1))0808)-$(date +%s -d today)))
+fi
+
 freepe=$(echo "scale=2; (($ramgb-$usedgb)/$ramgb)*100" | bc)
 echo "Today is $(date +%A), $(date +%d)th $(date +%B), $(date +%Y)"
-echo "There are X1 weeks Y1 days and Z1 hours left for the birthday of Dinesh
-There are X2 weeks Y2 days and Z2 hours left for the birthday of Nitish
-There are X3 weeks Y3 days and Z3 hours left for the birthday of Mahindar
+echo "There are $((${dineshsec}/604800)) weeks $(((${dineshsec}%((7*24)*3600))/(24*3600))) days and $(((${dineshsec}%(24*3600))/3600)) hours left for the birthday of Dinesh
+There are $((${nitishsec}/604800)) weeks $(((${nitishsec}%(7*24*3600))/(24*3600))) days and $(((${nitishsec}%(24*3600))/3600)) hours left for the birthday of Nitish
+There are $((${mahisec}/604800)) weeks $(((${mahisec}%(7*24*3600))/(24*3600))) days and $(((${mahisec}%(24*3600))/3600)) hours left for the birthday of Mahindar
 Thank you for asking, ${USER}
-Your system has been running for $(if [ -n $(uptime | sed -n 's/.* up \([0-9]*\) days,  .*$/\1/p') ]; then echo $(uptime | sed -n 's/.* up \([0-9]*\) days,  .*$/\1/p'); else echo "0"; fi) days, $(uptime | sed -n 's/[^:]*:[^:]*:[^:]*\(..:..\).*$/\1/p' | sed -n 's/^ /0/p') hours.
-The current disk on which your home folder is located is $(df | sed -n '2s/^\([^ ]*\).*$/p') and is $(df | sed -n '2s/^.*\(..\)%.*$/\1/p')% full
+Your system has been running for $(if [ $($(uptime | sed -n 's/.* up \([0-9]*\) days,  .*$/\1/p') | wc -w) -ne 0 ]; then echo $(uptime | sed -n 's/.* up \([0-9]*\) days,  .*$/\1/p'); else echo "0"; fi) days, $(uptime | sed -n 's/[^:]*:[^:]*:[^:]*\(..:..\).*$/\1/p' | sed 's/^ /0/p') hours.
+The current disk on which your home folder is located is $(df | sed -n '2s/^\([^ ]*\).*$/\1/p') and is $(df | sed -n '2s/^.*\(..\)%.*$/\1/p')% full
 You are running $(lsb_release -c -s) and $(lsb_release -r -s) with Kernel $(uname -r)
 Your machine has $ramgb GB RAM. Of which $freepe% is in use.
 This script has $(cat ./scripts/lab6scripts/myinfo.sh | wc -w) words, $(cat ./scripts/lab6scripts/myinfo.sh | wc -l) lines and $(($(cat ./scripts/lab6scripts/myinfo.sh | wc -w)-$(cat ./scripts/lab6scripts/myinfo.sh | wc -l))) characters (without counting whitespace)"
+
